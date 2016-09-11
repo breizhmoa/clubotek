5aa68a6a4b93c6f1088bb6295e487b39b1cfeaa0
Xavier Chaupitre <xavier.chaupitre@neticoa.fr>
2016-09-11 22:26:04 +0200

    add filter by country

--- src/app/controllers/ClubsController.js
+++ src/app/controllers/ClubsController.js
@@ -4,15 +4,17 @@
     .module('app')
     .controller('ClubsController', [
       'clubsService',
+      'countriesService',
       ClubsController
     ]);
 
-  function ClubsController(clubsService) {
+  function ClubsController(clubsService, countriesService) {
     var vm = this;
 
     vm.searchClub = ''; //'rennes';
+    vm.countries = countriesService.loadAll();
+    
     vm.clubsData = [];
-
     clubsService
       .loadAll()
       .then(function(clubsData) {
