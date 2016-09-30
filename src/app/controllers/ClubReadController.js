35981faacf481c55e8c6ced020d347af9927a2fd
Xavier Chaupitre <xavier.chaupitre@neticoa.fr>
2016-09-30 23:15:21 +0200

    update club details page

--- src/app/controllers/ClubReadController.js
+++ src/app/controllers/ClubReadController.js
@@ -23,6 +23,26 @@
     vm.trustSrc = function(src) {
         return $sce.trustAsResourceUrl(src);
     }
+
+    vm.getCompetitionRankFriendlyName = function(type, rank) {
+      if (type === 'cup') {
+        switch (rank) {
+          case 1: return 'vainqueur';
+          case 2: return 'finaliste';
+          case 3: case 4: return '1/2 finaliste';
+          case 5: case 6: case 7: case 8: return '1/2 finaliste';
+        }
+      }
+      else if (type === 'championship') {
+        switch (rank) {
+          case 1: return 'champion';
+          case 2: return '2nd';
+          default: return '' + rank + 'eme';
+        }
+      }
+
+      return '' + rank;
+    }
   }
 
 })();
